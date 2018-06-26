7f8ca54cb5de8c5ba5b60f2f39af8bd6de69ed43
mitchell budden <>
2018-06-26 19:08:05 -0400

    Adding firebase

---
+++ src/firebase.js
@@ -0,0 +1,12 @@
+import * as firebase from "firebase";
+
+const config = {
+  apiKey: "AIzaSyDsInAYyExeynDsRE_cdF11xatIkuj3JUQ",
+  authDomain: "goalcoach-bb5cf.firebaseapp.com",
+  databaseURL: "https://goalcoach-bb5cf.firebaseio.com",
+  projectId: "goalcoach-bb5cf",
+  storageBucket: "goalcoach-bb5cf.appspot.com",
+  messagingSenderId: "542818990624"
+};
+
+export const firebaseApp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -1,4 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { Router, Route, browserHistory } from "react-router";
+import { firebaseApp } from "./firebase";
+import App from "./components/App";
+import SignIn from "./components/SignIn";
+import SignUp from "./components/SignUp";
 
-ReactDOM.render(<div>App</div>, document.getElementById("root"));
+firebaseApp.auth().onAuthStateChanged(user => {
+  if (user) {
+    console.log("user has signed in", user);
+  } else {
+    console.log("user has not signed up");
+  }
+});
+
+ReactDOM.render(
+  <Router path="/" history={browserHistory}>
+    <Route path="/app" component={App} />
+    <Route path="/signin" component={SignIn} />
+    <Route path="/signup" component={SignUp} />
+  </Router>,
+  document.getElementById("root")
+);
